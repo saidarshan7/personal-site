4a259db5ca4aba3c3c6be3705e573ae49a3857f5
saidarshan74 <141834083+saidarshan7@users.noreply.github.com>
2025-10-19 17:14:38 +0530

    fix: handling some common errors in bashscript

--- automate.sh
+++ automate.sh
@@ -7,7 +7,10 @@
 hugo
 git status
 read -p "enter files you want to stage (ex: . for all files ) : "  filePath
-git add $filePath
+git add $filePath  || {
+	 echo "Error: enter correct file path !!!"
+         exit 1
+}
 read -p "enter yor commit message : " message
 echo "your commit : $message"
 git commit -m  "$message"
